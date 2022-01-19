13ed1849ffa80d07afff2e52188d6737b993c581
henrycunh <henrycunh@gmail.com>
2022-01-19 12:06:56 -0300

    feat: support transform operation name

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,73 @@
-import { Document } from "openapi-client-axios"
-import path from "path"
+import path from 'path'
 import fs from 'fs'
-import OASParser from "./parser"
-import OASTypeGenerator from "./typegen"
+import type { Document } from 'openapi-client-axios'
 import yaml from 'yaml'
+import { flattenReferences } from './parser'
+import OASTypeGenerator from './typegen'
 
+export const createClientFromOpenAPI = async(definition: string | Array<string> | Record<string, string>, serviceDirectoryPath: string, options: { transformOperationName?: (operation: string) => string }) => {
+    // this is ugly, i'm sorry
+    const definitionList
+            = Array.isArray(definition)
+                ? definition
+                : typeof definition === 'string'
+                    ? [definition]
+                    : Object.values(definition)
+    const fullServiceDirectoryPath = path.join(process.cwd(), serviceDirectoryPath)
+    const serviceTemplateContent = fs.readFileSync(path.join(__dirname, './template/client.template'), 'utf8')
 
-const OAS = {
-    async createClient(definition: string | Array<string> | {[serviceName: string]: string}, serviceDirectoryPath: string) {
-        // this is ugly, i'm sorry
-        const definitionList = 
-            Array.isArray(definition) ? 
-                definition : 
-                typeof definition === 'string' ?
-                    [definition] :
-                    Object.values(definition)
-        const fullServiceDirectoryPath = path.join(process.cwd(), serviceDirectoryPath)
-        const serviceTemplateContent = fs.readFileSync(path.join(__dirname, "./template/client.template"), "utf8")
+    if (!fs.existsSync(fullServiceDirectoryPath))
+        fs.mkdirSync(fullServiceDirectoryPath)
 
-        if (!fs.existsSync(fullServiceDirectoryPath)) {
-            fs.mkdirSync(fullServiceDirectoryPath)
-        }
+    for (const currentDefinition of definitionList) {
+        const serviceNameFromMap = typeof definition === 'object' && !Array.isArray(definition)
+            ? Object.keys(definition).find(key => definition[key] === currentDefinition)
+            : null
+        const serviceName = serviceNameFromMap || path.basename(currentDefinition, path.extname(currentDefinition))
+        const isDefinitionUrl = /^https?:\/\//.test(currentDefinition)
+        const definitionWithFlattenedReferences = !isDefinitionUrl ? (await flattenReferences(currentDefinition)) as Document : currentDefinition
+        const definitionTypes = (await OASTypeGenerator.generate(
+            isDefinitionUrl
+                ? currentDefinition
+                : definitionWithFlattenedReferences,
+            { transformOperationName: options.transformOperationName || ((operation: string) => operation) },
+        )).join('\n')
 
-        for (const currentDefinition of definitionList) {
-            const serviceNameFromMap = typeof definition === 'object' && !Array.isArray(definition) ? 
-                Object.keys(definition).find(key => definition[key] === currentDefinition) : 
-                null
-            const serviceName = serviceNameFromMap || path.basename(currentDefinition, path.extname(currentDefinition)) 
-            const isDefinitionUrl = /^https?:\/\//.test(currentDefinition)
-            const definitionWithFlattenedReferences = !isDefinitionUrl ? (await OASParser.flattenReferences(currentDefinition)) as Document : currentDefinition
-            const definitionTypes = (await OASTypeGenerator.generate(
-                isDefinitionUrl ? 
-                    currentDefinition : 
-                    definitionWithFlattenedReferences,
-                { transformOperationName: (operation: string) => operation }
-            )).join('\n')
+        // Create a directory for the service
+        const serviceDirectory = path.join(fullServiceDirectoryPath, serviceName)
+        if (!fs.existsSync(serviceDirectory))
+            fs.mkdirSync(serviceDirectory)
 
-            // Create a directory for the service
-            const serviceDirectory = path.join(fullServiceDirectoryPath, serviceName)
-            if (!fs.existsSync(serviceDirectory)) {
-                fs.mkdirSync(serviceDirectory)
-            }
-            const definitionWithFlattenedReferencesPath = path.join(serviceDirectory, `${serviceName}-definition.yml`)
+        const definitionWithFlattenedReferencesPath = path.join(serviceDirectory, `${serviceName}-definition.yml`)
 
-            // If the definition is not a url we must write to the service directory
-            if (!isDefinitionUrl) {
-                fs.writeFileSync(definitionWithFlattenedReferencesPath, yaml.stringify(definitionWithFlattenedReferences))
-            }
+        // If the definition is not a url we must write to the service directory
+        if (!isDefinitionUrl)
+            fs.writeFileSync(definitionWithFlattenedReferencesPath, yaml.stringify(definitionWithFlattenedReferences))
 
-            const definitionContentReplacement = 
-                isDefinitionUrl ?
-                    `'${currentDefinition}'` : 
-                    `\n\tJSON.parse(\n\t\t\`${JSON.stringify(
-                        definitionWithFlattenedReferences
+        const definitionContentReplacement
+                = isDefinitionUrl
+                    ? `'${currentDefinition}'`
+                    : `\n\tJSON.parse(\n\t\t\`${JSON.stringify(
+                        definitionWithFlattenedReferences,
                     )}\n\t\t\`) as Document`
 
-            // Consolidate the definition and the types into a single file
-            const serviceDefinitionContent = serviceTemplateContent
-                .replace(
-                    /\{definition\}/g, 
-                    definitionContentReplacement
-                )
-                
-            const serviceContentWithTypes = [
-                definitionTypes,
-                serviceDefinitionContent
-            ]
-            .join('\n')
+        // Consolidate the definition and the types into a single file
+        const serviceDefinitionContent = serviceTemplateContent
             .replace(
-                / +/g, ' '
+                /\{definition\}/g,
+                definitionContentReplacement,
             )
 
-            // Write the service definition
-            fs.writeFileSync(path.join(serviceDirectory, 'index.ts'), serviceContentWithTypes)
-        }
+        const serviceContentWithTypes = [
+            definitionTypes,
+            serviceDefinitionContent,
+        ]
+            .join('\n')
+            .replace(
+                / +/g, ' ',
+            )
+
+        // Write the service definition
+        fs.writeFileSync(path.join(serviceDirectory, 'index.ts'), serviceContentWithTypes)
     }
 }
-
-export default OAS
--- src/parser.ts
+++ src/parser.ts
@@ -3,30 +3,26 @@ import fs from 'fs'
 import $RefParser from '@apidevtools/json-schema-ref-parser'
 import yaml from 'yaml'
 
-export default class OASParser {
+export const flattenReferences = async(specificationSource: string, specificationDestination?: string): Promise<void | $RefParser.JSONSchema> => {
+    const sourceFullPath = path.join(process.cwd(), specificationSource)
+    const specificationContentRaw = fs.readFileSync(sourceFullPath, 'utf8')
+    const specificationContent = yaml.parse(specificationContentRaw)
 
-    static async flattenReferences(specificationSource: string, specificationDestination?: string): Promise<void | $RefParser.JSONSchema> {
-        const sourceFullPath = path.join(process.cwd(), specificationSource)
-        const specificationContentRaw = fs.readFileSync(sourceFullPath, 'utf8')
-        const specificationContent = yaml.parse(specificationContentRaw)
-        
-        // We need to map the references to the current path of the file
-        const mappedReferenceSpecificationContent = JSON.stringify(specificationContent)
-            .replace(
-                /"\$ref":\s*"([^#]+)#/g,
-                (_match, ref) => `"$ref":"${path.join(path.dirname(sourceFullPath), ref)}#`
-            )
-        const flattenedSpecificationContent = await $RefParser.dereference(JSON.parse(mappedReferenceSpecificationContent))
-        
-        // If the destination is not specified, we return the flattened specification
-        if (specificationDestination) {
-            const destinationFullPath = path.join(__dirname, specificationDestination)
-            fs.writeFileSync(destinationFullPath, yaml.stringify(flattenedSpecificationContent, {
-                indent: 4,
-            }))
-        }
+    // We need to map the references to the current path of the file
+    const mappedReferenceSpecificationContent = JSON.stringify(specificationContent)
+        .replace(
+            /"\$ref":\s*"([^#]+)#/g,
+            (_match, ref) => `"$ref":"${path.join(path.dirname(sourceFullPath), ref)}#`,
+        )
+    const flattenedSpecificationContent = await $RefParser.dereference(JSON.parse(mappedReferenceSpecificationContent))
 
-        return flattenedSpecificationContent
+    // If the destination is not specified, we return the flattened specification
+    if (specificationDestination) {
+        const destinationFullPath = path.join(__dirname, specificationDestination)
+        fs.writeFileSync(destinationFullPath, yaml.stringify(flattenedSpecificationContent, {
+            indent: 4,
+        }))
     }
 
+    return flattenedSpecificationContent
 }
